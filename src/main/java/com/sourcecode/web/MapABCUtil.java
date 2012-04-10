a8d441347eb1397530c3befd7028183ea1ef606d
toby941 <toby941@gmail.com>
2012-04-11 01:57:42 +0800

    test

--- src/main/java/com/sourcecode/web/MapABCUtil.java
+++ src/main/java/com/sourcecode/web/MapABCUtil.java
@@ -29,62 +29,71 @@ import org.slf4j.LoggerFactory;
  */
 public class MapABCUtil {
 
-    private static Logger logger = LoggerFactory.getLogger(MapABCUtil.class);
+	private static Logger logger = LoggerFactory.getLogger(MapABCUtil.class);
 
-    public final static Integer HTTP_RESPONSE_STATUS_SUCCESS_CODE = 200;
+	public final static Integer HTTP_RESPONSE_STATUS_SUCCESS_CODE = 200;
 
-    private final static String XML_URL =
-            "<?xml version=\"1.0\" encoding=\"gb2312\"?>\r\n"
-                    + "<spatial_request method=\"searchPoint\"><x>{0}</x><y>{1}</y><xs/><ys/><poiNumber>10</poiNumber><range>NaN</range><pattern>1</pattern><roadLevel>\r\n"
-                    + "0</roadLevel><exkey/></spatial_request>\r\n";
+	private final static String XML_URL = "<?xml version=\"1.0\" encoding=\"gb2312\"?>\r\n"
+			+ "<spatial_request method=\"searchPoint\"><x>{0}</x><y>{1}</y><xs/><ys/><poiNumber>10</poiNumber><range>NaN</range><pattern>1</pattern><roadLevel>\r\n"
+			+ "0</roadLevel><exkey/></spatial_request>\r\n";
 
-    public final static String[] API_KEY_ARRAY = new String[] { "b0a7db0b3a30f944a21c3682064dc70ef5b738b062f6479a5eca39725798b1ee300bd8d5de3a4ae3" };
+	public final static String[] API_KEY_ARRAY = new String[]{"b0a7db0b3a30f944a21c3682064dc70ef5b738b062f6479a5eca39725798b1ee300bd8d5de3a4ae3"};
 
-    private static String MAPABC_URL = "http://search1.mapabc.com/sisserver?config=SPAS&ver=2.0&resType=json&enc=utf-8&spatialXml={0}&a_k={1}";
+	private static String MAPABC_URL = "http://search1.mapabc.com/sisserver?config=SPAS&ver=2.0&resType=json&enc=utf-8&spatialXml={0}&a_k={1}";
 
-    public static String executeHttpGet(String requestUrl) throws ParseException, IOException {
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(requestUrl);
-        HttpResponse response = httpclient.execute(httpGet);
-        StatusLine statusLine = response.getStatusLine();
-        if (HTTP_RESPONSE_STATUS_SUCCESS_CODE.equals(statusLine.getStatusCode())) {
-            String result = EntityUtils.toString(response.getEntity());
-            return result;
-        } else {
-            logger.error("execute map abc error: " + ReflectionToStringBuilder.toString(statusLine));
-        }
-        return StringUtils.EMPTY;
-    }
+	public static String executeHttpGet(String requestUrl)
+			throws ParseException, IOException {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(requestUrl);
+		HttpResponse response = httpclient.execute(httpGet);
+		StatusLine statusLine = response.getStatusLine();
+		if (HTTP_RESPONSE_STATUS_SUCCESS_CODE
+				.equals(statusLine.getStatusCode())) {
+			String result = EntityUtils.toString(response.getEntity());
+			return result;
+		} else {
+			logger.error("execute map abc erro  r: "
+					+ ReflectionToStringBuilder.toString(statusLine));
+		}
+		return StringUtils.EMPTY;
+	}
 
-    public static boolean isValidLongitudeAndLatitude(String longitude, String latitude) {
-        return StringUtils.isNotBlank(longitude) && StringUtils.isNotBlank(latitude) && !"0".equals(longitude) && !"0".equals(latitude);
-    }
+	public static boolean isValidLongitudeAndLatitude(String longitude,
+			String latitude) {
+		return StringUtils.isNotBlank(longitude)
+				&& StringUtils.isNotBlank(latitude) && !"0".equals(longitude)
+				&& !"0".equals(latitude);
+	}
 
-    public static LocationInfo getLocationInfo(String longitude, String latitude) throws ParseException, IOException {
-        if (isValidLongitudeAndLatitude(longitude, latitude)) {
-            String xmlParameter = MessageFormat.format(XML_URL, longitude, latitude);
-            String apiKey = API_KEY_ARRAY[0];
-            String requestUrl = getMapABCRequestURL(xmlParameter, apiKey);
-            String result = executeHttpGet(requestUrl);
+	public static LocationInfo getLocationInfo(String longitude, String latitude)
+			throws ParseException, IOException {
+		if (isValidLongitudeAndLatitude(longitude, latitude)) {
+			String xmlParameter = MessageFormat.format(XML_URL, longitude,
+					latitude);
+			String apiKey = API_KEY_ARRAY[0];
+			String requestUrl = getMapABCRequestURL(xmlParameter, apiKey);
+			String result = executeHttpGet(requestUrl);
 
-            if (StringUtils.isNotBlank(result)) {
-                JSONObject jsonObject = JSONObject.fromObject(result);
-                LocationInfo locationInfo = new LocationInfo(jsonObject);
-                locationInfo.setLongitude(longitude);
-                locationInfo.setLatitude(latitude);
-                return locationInfo;
-            }
-        }
+			if (StringUtils.isNotBlank(result)) {
+				JSONObject jsonObject = JSONObject.fromObject(result);
+				LocationInfo locationInfo = new LocationInfo(jsonObject);
+				locationInfo.setLongitude(longitude);
+				locationInfo.setLatitude(latitude);
+				return locationInfo;
+			}
+		}
 
-        return LocationInfo.getEmptyLocationInfo();
-    }
+		return LocationInfo.getEmptyLocationInfo();
+	}
 
-    public static String getMapABCRequestURL(String xml, String apiKey) throws UnsupportedEncodingException {
-        return MessageFormat.format(MAPABC_URL, URLEncoder.encode(xml, "utf-8"), apiKey);
-    }
+	public static String getMapABCRequestURL(String xml, String apiKey)
+			throws UnsupportedEncodingException {
+		return MessageFormat.format(MAPABC_URL,
+				URLEncoder.encode(xml, "utf-8"), apiKey);
+	}
 
-    public static void main(String[] args) throws Exception {
-        LocationInfo locationInfo = getLocationInfo("118.735063", "32.042041");
-        System.out.println(locationInfo);
-    }
+	public static void main(String[] args) throws Exception {
+		LocationInfo locationInfo = getLocationInfo("118.735063", "32.042041");
+		System.out.println(locationInfo);
+	}
 }
