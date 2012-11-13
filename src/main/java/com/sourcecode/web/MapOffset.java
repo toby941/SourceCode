ff9f9f2c1155a7f88e7591463dd7470fe19cf1e4
toby <toby941@gmail.com>
2012-11-13 16:00:22 +0800

    map 经纬度修正

--- src/main/java/com/sourcecode/web/MapOffset.java
+++ src/main/java/com/sourcecode/web/MapOffset.java
@@ -1,6 +1,8 @@
 package com.sourcecode.web;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.MessageFormat;
 import java.text.ParseException;
 
 import org.apache.commons.lang.StringUtils;
@@ -71,7 +73,88 @@ public class MapOffset {
         return false;
     }
 
-    public static void main(String[] args) {
-        getOffset(39.111195, 117.148067);
+    public static void main(String[] args) throws IOException {
+        // x y 32.076566,118.747892
+        // 11874:3207:972:449
+        // fix1 118.75310621432493 32.07452505596113
+        // fix2 118.74267778567503 32.078606898476124
+        double lng = 118.747892;
+        double lat = 32.076566;
+        // double lngS = lngToPixel(lng, 18) + 972;
+        // System.out.println("lngS:" + lngS);
+        // double lngs = pixelToLng(lngS, 18);
+        // System.out.println("lngs:" + lngs);
+        // double lngX = latToPixel(lat, 18) + 449;
+        // System.out.println("lngX:" + lngX);
+        // double lngx = pixelToLat(lngX, 18);
+        // System.out.println("lngx:" + lngx);
+        // System.out.println(lngs + " " + lngx);
+
+        // double lngS = lngToPixel(lng, 18) - 972;
+        // System.out.println("lngS:" + lngS);
+        // double lngs = pixelToLng(lngS, 18);
+        // System.out.println("lngs:" + lngs);
+        // double lngX = latToPixel(lat, 18) - 449;
+        // System.out.println("lngX:" + lngX);
+        // double lngx = pixelToLat(lngX, 18);
+        // System.out.println("lngx:" + lngx);
+        // System.out.println(lngs + " " + lngx);
+        String path = "D:\\Dropbox\\doc\\mitian\\dev\\passbook\\google地图偏移精度5米.txt";
+        readToDB(path);
+
+    }
+
+    // 经度到像素X值
+    public static double lngToPixel(double lng, int zoom) {
+        return (lng + 180) * (256 << zoom) / 360;
+    }
+
+    // 像素X到经度
+    public static double pixelToLng(double pixelX, int zoom) {
+        return pixelX * 360 / (256 << zoom) - 180;
+    }
+
+    // 纬度到像素Y
+    public static double latToPixel(double lat, int zoom) {
+        double siny = Math.sin(lat * Math.PI / 180);
+        double y = Math.log((1 + siny) / (1 - siny));
+        return (128 << zoom) * (1 - y / (2 * Math.PI));
+    }
+
+    // 像素Y到纬度
+    public static double pixelToLat(double pixelY, int zoom) {
+        double y = 2 * Math.PI * (1 - pixelY / (128 << zoom));
+        double z = Math.pow(Math.E, y);
+        double siny = (z - 1) / (z + 1);
+        return Math.asin(siny) * 180 / Math.PI;
+    }
+
+    public static String sql_templete =
+            "insert into `EMMS_MAP_OFFSET`(`LNG`,`LAT`,`OFF_LNG`,`OFF_LAT`) values ({0},{1},{2},{3});\r\n";
+
+    public static void readToDB(String filePath) throws IOException {
+        RandomAccessFile read = new RandomAccessFile(filePath, "r");
+        RandomAccessFile writer = new RandomAccessFile("D://tmp//offset.sql", "rw");
+        int count = 0;
+        while (true) {
+            count++;
+            String s = read.readLine();
+            if (s == null) {
+                break;
+            }
+            if (count % 500 == 0) {
+                System.out.println("count:" + count);
+            }
+            else {
+                String trimValue = StringUtils.trimToEmpty(s);
+                if (StringUtils.isNotBlank(trimValue)) {
+                    String[] values = trimValue.split(":");
+                    String sql = MessageFormat.format(sql_templete, values);
+                    writer.write(sql.getBytes());
+                }
+            }
+        }
+        read.close();
+        writer.close();
     }
 }
