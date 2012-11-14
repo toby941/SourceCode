935d52146d482a6bd90d316ac0b7fb23dc20a423
toby <toby941@gmail.com>
2012-11-14 17:42:02 +0800

    Incomplete - task : EMMS

--- src/main/java/com/sourcecode/web/MapOffset.java
+++ src/main/java/com/sourcecode/web/MapOffset.java
@@ -73,13 +73,17 @@ public class MapOffset {
         return false;
     }
 
+    // 沙县小吃 32.075147, 118.753981 11875 3207 973 448 118.74876142125703 32.07718338470381
+    // 察哈尔车站 32.075541, 118.751902 118.74668242125699 32.07757737592766
+    // 金城路 32.075786, 118.749008 11874 3207 972 449 118.74379378567505 32.077826915889425
+
     public static void main(String[] args) throws IOException {
         // x y 32.076566,118.747892
         // 11874:3207:972:449
         // fix1 118.75310621432493 32.07452505596113
         // fix2 118.74267778567503 32.078606898476124
-        double lng = 118.747892;
-        double lat = 32.076566;
+        double lng = 118.749008;
+        double lat = 32.075786;
         // double lngS = lngToPixel(lng, 18) + 972;
         // System.out.println("lngS:" + lngS);
         // double lngs = pixelToLng(lngS, 18);
@@ -90,17 +94,17 @@ public class MapOffset {
         // System.out.println("lngx:" + lngx);
         // System.out.println(lngs + " " + lngx);
 
-        // double lngS = lngToPixel(lng, 18) - 972;
-        // System.out.println("lngS:" + lngS);
-        // double lngs = pixelToLng(lngS, 18);
-        // System.out.println("lngs:" + lngs);
-        // double lngX = latToPixel(lat, 18) - 449;
-        // System.out.println("lngX:" + lngX);
-        // double lngx = pixelToLat(lngX, 18);
-        // System.out.println("lngx:" + lngx);
-        // System.out.println(lngs + " " + lngx);
-        String path = "D:\\Dropbox\\doc\\mitian\\dev\\passbook\\google地图偏移精度5米.txt";
-        readToDB(path);
+        double lngS = lngToPixel(lng, 18) - 972;
+        System.out.println("lngS:" + lngS);
+        double lngs = pixelToLng(lngS, 18);
+        System.out.println("lngs:" + lngs);
+        double lngX = latToPixel(lat, 18) - 449;
+        System.out.println("lngX:" + lngX);
+        double lngx = pixelToLat(lngX, 18);
+        System.out.println("lngx:" + lngx);
+        System.out.println(lngs + " " + lngx);
+        // String path = "D:\\Dropbox\\doc\\mitian\\dev\\passbook\\google地图偏移精度5米.txt";
+        // readToDB(path);
 
     }
 
